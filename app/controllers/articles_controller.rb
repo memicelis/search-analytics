1122a78d85b9f01e168c31c699095be62f5f3bcc
memicelis <elismemic5@gmail.com>
2024-01-05 00:21:31 +0100

    Create search form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,11 +3,16 @@ class ArticlesController < ApplicationController
 
   # GET /articles or /articles.json
   def index
-    @articles = Article.all
+    if params[:query].present?
+      @articles = Article.where("title ILIKE ?", "%#{params[:query].split(' ').join('%')}%")
+    else
+      @articles= Article.all
+    end
   end
 
   # GET /articles/1 or /articles/1.json
   def show
+    @article = Article.find(params[:id])
   end
 
   # GET /articles/new
@@ -22,17 +27,14 @@ class ArticlesController < ApplicationController
   # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+  
+    if @article.save
+      redirect_to articles_path, notice: 'Article was successfully created.'
+    else
+      render :new
     end
   end
+  
 
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
@@ -58,13 +60,11 @@ class ArticlesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_article
       @article = Article.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def article_params
-      params.fetch(:article, {})
+      params.require(:article).permit(:title)
     end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,3 @@
 class Article < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 1, maximum: 100 } 
 end
