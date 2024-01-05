1fedcb2dbdece95f8884f58a7efb174687f73684
memicelis <elismemic5@gmail.com>
2024-01-05 18:05:50 +0100

    Add migrations and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_04_183411) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_04_232917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,4 +20,19 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_04_183411) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "searches", force: :cascade do |t|
+    t.string "query"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_searches_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "ip_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "searches", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+article_titles = [
+    "The Cathedral and the Bazaar",
+    "Reflections on Trusting Trust",
+    "No Silver Bullet — Essence and Accidents of Software Engineering",
+    "The Mythical Man-Month",
+    "You and Your Research",
+    "The Coming Software Apocalypse",
+    "The Turing Test",
+    "A Few Useful Things to Know About Machine Learning",
+    "Out of the Tar Pit",
+    "The Pragmatic Programmer: Your Journey to Mastery"
+  ]
+  
+  article_titles.each do |title|
+    Article.create(title: title)
+  end
