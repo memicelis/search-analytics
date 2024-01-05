a62bfbc16303e93b0a3387965e547f5f4b028d7e
memicelis <elismemic5@gmail.com>
2024-01-05 18:07:51 +0100

    Update controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,70 +1,46 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show edit update destroy ]
+  before_action :set_article, only: %i[show edit update destroy]
 
-  # GET /articles or /articles.json
   def index
-    if params[:query].present?
-      @articles = Article.where("title ILIKE ?", "%#{params[:query].split(' ').join('%')}%")
-    else
-      @articles= Article.all
-    end
+    @articles = if params[:query].present?
+                  Article.where("title ILIKE ?", "%#{params[:query].split(' ').join('%')}%")
+                else
+                  Article.all
+                end
+
+    @search_query = params[:query]
+    Search.update_search_query(current_user, @search_query)
   end
 
-  # GET /articles/1 or /articles/1.json
-  def show
-    @article = Article.find(params[:id])
-  end
-
-  # GET /articles/new
-  def new
-    @article = Article.new
-  end
-
-  # GET /articles/1/edit
-  def edit
-  end
-
-  # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
-  
+
     if @article.save
       redirect_to articles_path, notice: 'Article was successfully created.'
     else
       render :new
     end
   end
-  
 
-  # PATCH/PUT /articles/1 or /articles/1.json
-  def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /articles/1 or /articles/1.json
   def destroy
     @article.destroy
 
     respond_to do |format|
-      format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
+      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    def set_article
-      @article = Article.find(params[:id])
-    end
 
-    def article_params
-      params.require(:article).permit(:title)
-    end
-end
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title)
+  end
+end 
+  
+  
+
